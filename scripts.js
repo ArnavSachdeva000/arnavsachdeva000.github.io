894fea93671d08b88fbf5531eccd3b4ce3cfc1e7
ArnavSachdeva000 <arnavsachdeva@proton.me>
2024-08-02 15:05:58 +0530

    Update

--- scripts.js
+++ scripts.js
@@ -16,6 +16,13 @@ function toggleMenu() {
     menu.classList.toggle('show');
 }
 
+// Show/hide About Me section
+document.getElementById('about-me-link').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor link behavior
+    const aboutMeSection = document.getElementById('about-me');
+    aboutMeSection.style.display = aboutMeSection.style.display === 'block' ? 'none' : 'block';
+});
+
 // Initialize the page to show the correct menu button based on the default mode
 document.addEventListener('DOMContentLoaded', () => {
     if (document.body.classList.contains('dark-mode')) {
